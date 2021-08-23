46f32d1465e73631c78661e921f0ff67a0d9ab39
shmel-kai <al88.korotkevich@gmail.com>
2021-08-23 19:53:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  const sum = array.reduce(function(acc, curr){
+    return acc + curr;
+  }, 0)
+  return sum / array.length;
 }
